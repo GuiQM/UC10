0b51f469d7684a19e5089336df2ef5e962c44aa3
GuiQM <162368564+GuiQM@users.noreply.github.com>
2024-10-22 15:07:33 -0300

    Delete JogoBattle directory

--- JogoBattle/app/src/main/java/JogoBattle/Guerreiro.java
+++
@@ -1,9 +0,0 @@
-package JogoBattle;
-
-
-public class Guerreiro extends Personagem{
-
-public Guerreiro(String nome){
-super(nome, 100, 15);
-}
-}
--- JogoBattle/app/src/main/java/JogoBattle/Main.java
+++
@@ -1,9 +0,0 @@
-package JogoBattle;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Game game = new Game(); //Inicializa o jogo
-        game.start(); // inicia o jogo
-    }
-}
